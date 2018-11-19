8505b8f531081464085778dd3a1d4b8637b7b4e0
Halushka <andriy.halushka@ekreative.com>
2018-11-19 12:36:27 +0200

    added no-lifecycle fetch event listener

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,8 @@ self.addEventListener('activate', e => {
   console.log('[Service Worker] Activating...', e)
   return self.clients.claim()
 })
+
+self.addEventListener('fetch', e => {
+  console.log('[Service Worker] Fetching...', e)
+  e.respondWith(fetch(e.request))
+})
