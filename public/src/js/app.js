db2edc5d573ea787cf90607ef96c32fbed5cecc1
Halushka <andriy.halushka@ekreative.com>
2018-11-19 14:24:35 +0200

    catching and controll app install banner

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,13 @@
+let deferredPrompt
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(() => console.log('Service worker registered'))
 }
+
+window.addEventListener('beforeinstallprompt', e => {
+  console.log('beforeinstallprompt fired')
+  deferredPrompt = e
+  return false
+})
