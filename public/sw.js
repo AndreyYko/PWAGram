6d2ba78dff4e16b2d17c4ed370a75bc59d71c9e1
Andrii Halushka <andrushagalushka@gmail.com>
2019-01-06 11:30:10 +0200

    applying cache oly strategy to service worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,21 @@
-const CACHE_STATIC_NAME = 'static-v10'
-const CACHE_DYNAMIC_NAME = 'dynamic-v3'
+const CACHE_STATIC_NAME = 'static-v4'
+const CACHE_DYNAMIC_NAME = 'dynamic-v4'
+const STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/fetch.js',
+  '/src/js/promise.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+]
 
 self.addEventListener('install', e => {
   console.log('[Service Worker] Installing...', e)
@@ -7,22 +23,7 @@ self.addEventListener('install', e => {
     caches.open(CACHE_STATIC_NAME)
       .then(cache => {
         console.log('[Service Worker] Precaching App Shell')
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/offline.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/fetch.js',
-          '/src/js/promise.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ])
+        return cache.addAll(STATIC_FILES)
       })
   )
 })
@@ -56,6 +57,8 @@ self.addEventListener('fetch', e => {
             })
         })
     )
+  } else if (STATIC_FILES.includes(e.request.url)) {
+    e.respondWith(caches.match(e.request))
   } else {
     e.respondWith(
       caches.match(e.request)
@@ -72,7 +75,7 @@ self.addEventListener('fetch', e => {
             .catch(err => {
               return caches.open(CACHE_STATIC_NAME)
                 .then(cache => {
-                  if (e.request.url.indexOf('/help') > -1) {
+                  if (e.request.headers.get('accept').includes('text/html')) {
                     return cache.match('/offline.html')
                   }
                 })
